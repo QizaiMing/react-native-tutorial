e7e96f6bb7a30114e5411cb291564d4fe031c7f1
Juan Chacon <mahnimex@gmail.com>
2021-02-14 17:36:13 -0400

    Started using basic components and styling

--- App.tsx
+++ App.tsx
@@ -1,19 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
-import React, { useState } from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Button, StyleSheet, TextInput, View } from 'react-native'
 
 export default function App() {
-  const [outputText, setOutputText] = useState<string>(
-    'Open up App.tsx to start working on your app!'
-  )
-
   return (
     <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        title='Change Text'
-        onPress={() => setOutputText('The text changed')}
-      />
+      <View style={styles.section}>
+        <TextInput placeholder='Course Goal' style={styles.textInput} />
+        <Button title='ADD' onPress={() => null} />
+      </View>
       <StatusBar style='auto' />
     </View>
   )
@@ -21,9 +16,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    padding: 50
+  },
+  textInput: {
+    borderColor: 'black',
+    width: '80%',
+    borderWidth: 1,
+    padding: 10
+  },
+  section: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   }
 })
